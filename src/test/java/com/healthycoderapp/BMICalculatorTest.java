f671e12057ad5110a8c0ac6927279be677a80c50
Thuyen Code <thuyencode@proton.me>
2024-04-12 21:12:20 +0700

    test(app): Test with multiple assertions

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,8 +1,13 @@
 package com.healthycoderapp;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -45,5 +50,22 @@ public class BMICalculatorTest {
 
     // Then
     assertThrows(ArithmeticException.class, executable);
+  }
+
+  @Test
+  void should_ReturnCoderWithWorstBMI_When_CoderListNotEmpty() {
+    // Given
+    List<Coder> coders = new ArrayList<>();
+    coders.add(new Coder(1.8, 60));
+    coders.add(new Coder(1.82, 98));
+    coders.add(new Coder(1.82, 64.7));
+
+    // When
+    Coder coderWithWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+
+    // Then
+    assertAll(
+        () -> assertEquals(1.82, coderWithWorstBMI.getHeight()),
+        () -> assertEquals(98, coderWithWorstBMI.getWeight()));
   }
 }
