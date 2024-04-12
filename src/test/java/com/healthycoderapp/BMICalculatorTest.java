0508c83636d6d3b65fc02b3104894380d42e5096
Thuyen Code <thuyencode@proton.me>
2024-04-12 19:21:13 +0700

    test(app): Add the first test file

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,12 @@
+package com.healthycoderapp;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class BMICalculatorTest {
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+}
