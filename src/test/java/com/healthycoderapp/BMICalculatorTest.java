f72e89a2eea4b438fbf4e1fba37fe0ed7d148313
Thuyen Code <thuyencode@proton.me>
2024-04-12 20:42:17 +0700

    test(app): Complete the first test

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,12 +1,12 @@
 package com.healthycoderapp;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class BMICalculatorTest {
   @Test
   void test() {
-    fail("Not yet implemented");
+    assertTrue(BMICalculator.isDietRecommended(89, 1.72));
   }
 }
